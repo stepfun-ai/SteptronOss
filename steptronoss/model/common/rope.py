6319077270134506a616a3504a016ff7fd6b0045
zhouhy <rndj@outlook.com>
2026-02-28 11:16:23 +0800

    fix: rope get_cos_sin_cache with CP

--- steptronoss/model/common/rope.py
+++ steptronoss/model/common/rope.py
@@ -4,6 +4,7 @@ import torch
 from loguru import logger
 from torch import Tensor
 
+from steptronoss.core.parallel_state import PM
 from steptronoss.exp.abstract import ModelConfig as AbstractModelConfig
 
 
@@ -201,12 +202,12 @@ class YARNRoPE(torch.nn.Module):
         # position_id: [S, ]
 
         if position_id is not None:  # packed sample
-            max_seqlen = feature.shape[1]
+            max_seqlen = position_id.max() + 1
             cos_cache, sin_cache = self._check_set_cos_sin_cache(max_seqlen, position_id.device)
             cos = cos_cache[None, position_id, None, :].to(feature.device).to(feature.dtype)
             sin = sin_cache[None, position_id, None, :].to(feature.device).to(feature.dtype)
         else:  # no packing
-            max_seqlen = feature.shape[1]
+            max_seqlen = feature.shape[1] * PM.size_of("CP")
             cos_cache, sin_cache = self._check_set_cos_sin_cache(max_seqlen, feature.device)
             cos = cos_cache[None, :max_seqlen, None, :].to(feature.device)
             sin = sin_cache[None, :max_seqlen, None, :].to(feature.device)
